3fffa1af13649a1fc14132ad400de586e63ef923
YasharBekov <35481559+YasharBekov@users.noreply.github.com>
2023-12-03 21:34:17 +0400

    first commit

---
+++ FirstLessonProgramming/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace FirstLessonProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first number: a=");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter second number: b=");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine(a + " + " + b + " = " + Sum(a, b));
+        }
+
+        public static int Sum(int var1, int var2)
+        {
+            return var1 + var2;
+        }
+    }
+}
